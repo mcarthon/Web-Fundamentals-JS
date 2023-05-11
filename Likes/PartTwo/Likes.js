8ba9245bb51838ae0943cf3bd8cfa4f99d417569
Mark Carthon <macarthoniii@gmail.com>
2023-05-10 20:59:26 -0400

    finished the logic for the js

--- Likes/PartTwo/Likes.js
+++ Likes/PartTwo/Likes.js
@@ -1,5 +1,12 @@
 var likesArray = [9, 9, 9];
+let classArray = [
+    document.querySelector("#like-count-0"),
+    document.querySelector("#like-count-1"),
+    document.querySelector("#like-count-2")
+];
 
-function addLikes() {
-    if 
+
+function addLikes(idx) {
+    likesArray[idx]++;
+    classArray[idx].innerText = `${likesArray[idx]} like(s)`;
 }
